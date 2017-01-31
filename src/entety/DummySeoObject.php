1f89800b537c244add1e00fcf757fcbe0b8c30b0
Dmitriy Ronzhin <hector.tmb@gmail.com>
2017-01-31 21:20:59 +0300

    init

---
+++ src/entety/DummySeoObject.php
@@ -0,0 +1,6 @@
+<?php
+
+class DummySeoObject implements interfaceSeo
+{
+
+}
---
+++ src/entety/interfaceSeo.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Interface interfaceSeo
+ */
+interface interfaceSeo
+{
+
+    /**
+     * @return string
+     */
+    public function getSlug();
+
+    /**
+     * @return string
+     */
+    public function getFullUrl();
+
+}
